fcb1e4a41877a7d7b83885beab8fc7cb9be09fef
Shen Mi <wohenshenmi@gmail.com>
2018-10-18 06:49:29 +0000

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,12 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::resource(
+    'users',
+    'UsersController',
+    ['only' => ['show', 'edit', 'update']]
+);
