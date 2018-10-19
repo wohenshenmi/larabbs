972248b6d2007d725cee0c74d58aa385922de8a2
Shen Mi <wohenshenmi@gmail.com>
2018-10-19 09:56:33 +0000

    Topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,52 +9,65 @@ use App\Http\Requests\TopicRequest;
 
 class TopicsController extends Controller
 {
+
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-		$topics = Topic::paginate();
-		return view('topics.index', compact('topics'));
-	}
+    public function index()
+    {
+        $topics = Topic::with('user', 'category')->paginate(30);
+
+        return view('topics.index', compact('topics'));
+    }
 
     public function show(Topic $topic)
     {
         return view('topics.show', compact('topic'));
     }
 
-	public function create(Topic $topic)
-	{
-		return view('topics.create_and_edit', compact('topic'));
-	}
+    public function create(Topic $topic)
+    {
+        return view('topics.create_and_edit', compact('topic'));
+    }
 
-	public function store(TopicRequest $request)
-	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
-	}
+    public function store(TopicRequest $request)
+    {
+        $topic = Topic::create($request->all());
 
-	public function edit(Topic $topic)
-	{
+        return redirect()->route('topics.show', $topic->id)->with(
+            'message',
+            'Created successfully.'
+        );
+    }
+
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
-	}
 
-	public function update(TopicRequest $request, Topic $topic)
-	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+        return view('topics.create_and_edit', compact('topic'));
+    }
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
-	}
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+        return redirect()->route('topics.show', $topic->id)->with(
+            'message',
+            'Updated successfully.'
+        );
+    }
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
+
+        return redirect()->route('topics.index')->with(
+            'message',
+            'Deleted successfully.'
+        );
+    }
 }
