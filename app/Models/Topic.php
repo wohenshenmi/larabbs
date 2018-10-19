0614c01b8cb4c9603653adf5536b348bccedca7c
Shen Mi <wohenshenmi@gmail.com>
2018-10-19 14:15:08 +0000

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -27,4 +27,28 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent':
+                $query = $this->recent();
+                break;
+            default:
+                $query = $this->recentReplied();
+                break;
+        }
+
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 }
